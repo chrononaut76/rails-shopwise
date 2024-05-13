0265eedaa6a48cb00159ed288d4c7539c02e5a7b
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-13 15:14:00 -0500

    Added faker, open-uri, and nokogiri gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# require 'faker'
+require 'open-uri'
+require 'nokogiri'
+
+# unless User.all.empty?
+#   puts "Purging 'Users' table..."
+#   User.all.each do |user|
+#     User.destroy(user.id)
+#     puts "  Records remaining: #{User.count}"
+#   end
+# end
+
+# puts 'Creating users...'
+# 2.times do
+#   puts "  Created #{User.create!(email: Faker::Internet.email, password: '123456').email}"
+# end
+# puts "Seeding users complete!"
+
+# unless Store.all.empty?
+#   puts "Purging 'Stores' table..."
+#   Store.all.each do |store|
+#     Store.destroy(store.id)
+#     puts "  Records remaining: #{Store.count}"
+#   end
+# end
+
+# puts 'Creating stores...'
+# # 2.times do
+# puts "  Created #{Store.create!(
+#   name: 'Metro Plus De la Montagne',
+#   address: '1230 Rue Notre-Dame Ouest, Montréal, QC H3C 1K6',
+#   image_url: 'http://localhost:3000',
+#   longitude: 0,
+#   latitude: 0
+# ).name}"
+# # end
+# puts "Seeding stores complete!"
+
+url = 'https://www.metro.ca/en/online-grocery/search-page-1'
+begin
+  response = URI.open(url).read
+  xml_doc = Nokogiri::HTML(response)
+  p xml_doc
+  # xml_doc.search('.content__head').first do |item|
+  #   puts item
+  # end
+rescue StandardError => e
+  print e
+end
