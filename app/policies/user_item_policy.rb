798cf7a1df3b4fb1727b5eed233acae24d101d96
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-05-14 20:34:44 -0400

    my items view

--- app/policies/user_item_policy.rb
+++ app/policies/user_item_policy.rb
@@ -1,4 +1,4 @@
-class UserItemsPolicy < ApplicationPolicy
+class UserItemPolicy < ApplicationPolicy
   # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
   # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
   # In most cases the behavior will be identical, but if updating existing
