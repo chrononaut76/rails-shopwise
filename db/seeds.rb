366762e8d79aa2cd5ca8d41361c58d918768bf2d
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-14 21:40:13 -0500

    Debugging seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,82 +7,87 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-# require 'faker'
 require 'open-uri'
 require 'json'
-# require 'watir'
-# require 'nokogiri'
+require 'faker'
 
+# Purge contents of 'Users' table
+unless User.all.empty?
+  puts "Purging 'Users' table..."
+  User.all.each do |user|
+    User.destroy(user.id)
+    puts "  Records remaining: #{User.count}"
+  end
+end
+
+# Seed 'Users' table
+puts 'Creating users...'
+3.times do
+  puts "  Created #{User.create!(email: Faker::Internet.email, password: '123456').email}"
+end
+puts 'Seeding users complete!'
+
+# Purge contents of 'Stores' table
+unless Store.all.empty?
+  puts "Purging 'Stores' table..."
+  Store.all.each do |store|
+    Store.destroy(store.id)
+    puts "  Records remaining: #{Store.count}"
+  end
+end
+
+# Seed 'Stores' table
+puts 'Creating stores...'
+5.times do
+  puts "  Created #{Store.create!(
+    name: Faker::Commerce.vendor,
+    address: Faker::Address.full_address,
+    image_url: Faker::Internet.url,
+    longitude: Faker::Address.longitude,
+    latitude: Faker::Address.latitude
+  ).name}"
+end
+puts 'Seeding stores complete!'
+
+# Purge contents of 'Items' table
+unless Item.all.empty?
+  puts "Purging 'Items' table..."
+  Item.all.each do |item|
+    Item.destroy(item.id)
+    puts "  Records remaining: #{Item.count}"
+  end
+end
+
+# Seed 'Items' table
+puts 'Creating items...'
 # url = "https://api.edamam.com/api/food-database/v2/parser?app_id=EDAMAM_APP_ID&app_key=EDAMAM_API_KEY&nutrition-type=cooking"
 url = 'https://api.edamam.com/api/food-database/v2/parser?app_id=d8a7c33c&app_key=a657d89a37f29e8c714e10004c9f3613&nutrition-type=cooking'
-until url.nil? do
+5.times do
   response = URI.open(url).read
   json = JSON.parse(response)
   json['hints'].each { |item| p Item.create!(name: item.dig('food', 'knownAs')) }
   url = json.dig('_links', 'next', 'href')
 end
-# def parser(url)
-#   return URI.open(url).read
-# rescue StandardError => e
-#   puts "#{e}. Trying again..."
-#   sleep 2
-#   parser(url)
-# end
+puts 'Seeding items complete!'
 
-# puts 'Creating browser instance'
-# browser = Watir::Browser.new
-# puts "Going to #{url}"
-# browser.goto(url)
-# puts 'Waiting for CSS selector presence'
-# begin
-#   js_doc = browser.elements(css: '.item-product__content') { |element| element.wait_until(&:present?) }
-# rescue StandardError => e
-#   puts e
-# end
-# puts 'Parsing data via Nokogiri'
-# xml_doc = js_doc.each { |element| Nokogiri::HTML(element.inner_html) }
-# puts 'Writing results to file'
-# File.open('iga_grocery_items.html', 'wb') do |file|
-#   xml_doc.each do |element|
-#     file.write(element.title)
-#   end
-# end
+# Purge contents of 'UserItems' table
+unless UserItem.all.empty?
+  puts "Purging 'UserItems' table..."
+  UserItem.all.each do |user_item|
+    UserItem.destroy(user_item.id)
+    puts "  Records remaining: #{UserItem.count}"
+  end
+end
 
-# xml_doc.each do |element|
-#   p element
-#   element.search('.js-ga-productname') { |item| p item.text }
-# end
-
-# unless User.all.empty?
-#   puts "Purging 'Users' table..."
-#   User.all.each do |user|
-#     User.destroy(user.id)
-#     puts "  Records remaining: #{User.count}"
-#   end
-# end
-
-# puts 'Creating users...'
-# 2.times do
-#   puts "  Created #{User.create!(email: Faker::Internet.email, password: '123456').email}"
-# end
-# puts "Seeding users complete!"
-
-# unless Store.all.empty?
-#   puts "Purging 'Stores' table..."
-#   Store.all.each do |store|
-#     Store.destroy(store.id)
-#     puts "  Records remaining: #{Store.count}"
-#   end
-# end
-
-# puts 'Creating stores...'
-# # 2.times do
-# puts "  Created #{Store.create!(
-#   name: 'Metro Plus De la Montagne',
-#   address: '1230 Rue Notre-Dame Ouest, Montréal, QC H3C 1K6',
-#   image_url: 'http://localhost:3000',
-#   longitude: 0,
-#   latitude: 0
-# ).name}"
-# # end
-# puts "Seeding stores complete!"
+# Seed 'UserItems' table
+puts 'Creating user items...'
+User.all.each do |user|
+  5.times do
+    user_item = UserItem.create!(
+      item_id: Item.find((Item.first.id..Item.last.id).to_a.sample),
+      user_id: user.id
+    )
+    puts "  Created #{Item.find(user_item.item_id).name} for #{user.email}"
+  end
+end
+puts 'Seeding user items complete!'
