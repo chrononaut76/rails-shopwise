905898bd60913a6affdd0f8f1159bb27b731e149
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-21 12:18:57 -0500

    rails g pundit:policy store_item

---
+++ test/policies/store_item_policy_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class StoreItemPolicyTest < ActiveSupport::TestCase
+  def test_scope
+  end
+
+  def test_show
+  end
+
+  def test_create
+  end
+
+  def test_update
+  end
+
+  def test_destroy
+  end
+end
