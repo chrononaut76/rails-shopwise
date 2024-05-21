bd64ade9733ae553221e61eaac37f64d90455748
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-21 17:29:02 -0500

    Coding scraper

--- app/controllers/store_items_controller.rb
+++ app/controllers/store_items_controller.rb
@@ -2,4 +2,27 @@ class StoreItemsController < ApplicationController
   def index
     @store_items = policy_scope(StoreItem)
   end
+
+  private
+
+  def fetch_prices(*terms)
+    stores_queries = {
+      iga: {
+        query: 'https://www.iga.net/en/search?k=',
+        selector: '.price'
+      },
+      metro: {
+        query: 'https://www.metro.ca/en/online-grocery/search?filter=',
+        selector: 'span .price-update'
+      },
+      pa: {
+        query: 'https://www.supermarchepa.com/search?q=',
+        selector: 'div .grid-product-price'
+      },
+      provigo: {
+        query: 'https://www.provigo.ca/search?search-bar=',
+        selector: '.price__value .selling-price-list__item__price .selling-price-list__item__price--sale__value'
+      }
+    }
+  end
 end
