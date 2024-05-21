84d10b19eb9325fe7d783027a9362948e96909c2
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-20 21:21:26 -0500

    Adding items to list now works

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,6 +1,4 @@
 class UserItemsController < ApplicationController
-  # before_action :set_item, only: %i[new create]
-
   def index
     @user_items = policy_scope(UserItem)
     @items = policy_scope(Item)
@@ -14,32 +12,17 @@ class UserItemsController < ApplicationController
   end
 
   def create
-
-    @item = Item.new(user_item_params)
-    @item.save!
     @user_item = UserItem.new
     @user_item.user = current_user
-    @user_item.item = @item
+    @user_item.item = Item.find(params[:item_id])
     authorize @user_item
-    if @user_item.save
-      redirect_to my_items_path, notice: 'Item added successfully.'
-    end
+    redirect_to my_items_path, notice: 'Item added successfully.' if @user_item.save!
   end
 
   def destroy
     @user_item = UserItem.find(params[:id])
     authorize @user_item
     @user_item.destroy
-    redirect_to my_items_path
-  end
-
-  private
-
-  # def set_item
-  #   @item = Item.find(params[:item_id])
-  # end
-
-  def user_item_params
-    params.require("/user_items").permit("/user_items", :name) # TODO: Identify required params
+    redirect_to my_items_path, status: :see_other
   end
 end
