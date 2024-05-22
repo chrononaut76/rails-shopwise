6de6771608ba232c970d294bccf8bb4e2a5c1dff
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-05-21 20:18:36 -0500

    Updating selectors for each query

--- scraper.rb
+++ scraper.rb
@@ -10,28 +10,33 @@ def fetch_prices(term)
     },
     metro: {
       query: 'https://www.metro.ca/en/online-grocery/search?filter=',
-      selector: 'span .price-update'
+      selector: '.price-update'
     },
     pa: {
       query: 'https://www.supermarchepa.com/search?q=',
-      selector: 'div .grid-product-price'
+      selector: '.grid-product-price'
     },
     provigo: {
       query: 'https://www.provigo.ca/search?search-bar=',
       selector: '.price__value .selling-price-list__item__price .selling-price-list__item__price--sale__value'
     }
   }
-  puts 'Creating new browser instance'
-  browser = Watir::Browser.new
-  url = "#{stores_queries.dig(:pa, :query)}#{term}"
-  puts "Navigating to #{url}"
-  browser.goto(url)
-  puts "Waiting for page to load selector #{stores_queries.dig(:pa, :selector)}"
-  js_doc = browser.element(css: stores_queries.dig(:pa, :selector)).wait_until(&:present?)
-  puts 'Printing content parsed by Nokogiri'
-  p content = Nokogiri::HTML(js_doc.inner_html)
-  puts 'Returning results'
-  return content.class
+  stores_queries.each_key do |key|
+    puts 'Creating new browser instance'
+    browser = Watir::Browser.new
+    url = "#{stores_queries.dig(key, :query)}#{term}"
+    puts "Navigating to #{url}"
+    browser.goto(url)
+    selector = stores_queries.dig(key, :selector)
+    puts "Waiting for page to load selector '#{selector}'"
+    begin
+      js_doc = browser.element(css: selector).wait_until(&:present?)
+      puts "Printing content parsed by Nokogiri for term '#{term}'"
+      p Nokogiri::HTML(js_doc.inner_html).text
+    rescue StandardError => e
+      puts e
+    end
+  end
 end
 
-puts fetch_prices('chicken')
+fetch_prices('sugar')
