df49e2e1b7bff2a8084dc41fe1e9ce2814d539a7
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-05-23 21:03:19 -0400

    accessing prices of StoreItems

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -4,6 +4,7 @@ class UserItemsController < ApplicationController
     @items = policy_scope(Item)
 
     @items = Item.search_by_name(params[:query]) if params[:query].present?
+    @stores = policy_scope(Store)
 
     respond_to do |format|
       format.html
@@ -33,3 +34,35 @@ end
 # separate the items by store they belong to
 # sum the prices by store
 # sort ascending
+
+def calculate
+  @stores = Store.all
+  @user_items = UserItem.where(user_id: 1)
+
+  # @stores.each do
+  # StoreItem.find_by(item_id: 57, store_id: 5).price
+
+
+
+
+end
+
+
+
+
+# def calculate_total_price(store_prices)
+#   total_price = 0
+#   product.each do |item|
+#     total_price += store_prices[item]
+#   end
+#   total_price
+# end
+
+
+
+
+
+# store_prices = { apple: 2, banana: 1, orange: 1.5 }
+
+# total_price = calculate_total_price(product, store_prices)
+# puts "Total price: $#{total_price}"
