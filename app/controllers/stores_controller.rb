94b13d9cb9967ea81f1fb544d75cce0ba3d2070a
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-05-25 13:06:26 -0400

    user list creation

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,14 @@
 class StoresController < ApplicationController
+
+  def compare_prices
+    @list = list_params
+    raise
+  end
+
   def results
+    @list = list_params
+    raise
+
     @stores = if params[:latitude].present? && params[:longitude].present?
                 Store.nearby(params[:latitude], params[:longitude])
               else
@@ -10,6 +19,13 @@ class StoresController < ApplicationController
       total_price = store.store_items.sum(:price)
       { store: store, total_price: total_price }
     end
+    # @stores_with_prices.sort-by!(&:price)
     authorize @stores
   end
+
+private
+
+  def list_params
+    params.require(:list).permit(:item_id, :user_id, :id)
+    end
 end
