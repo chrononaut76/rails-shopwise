f45e0992b95ced24c9279b32bb52836241deedcc
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-05-25 16:01:58 -0400

    fixed routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get 'my_items', to: 'user_items#index', as: 'my_items'
-  post 'my_items', to: 'user_items#create'
-  delete 'my_items/:id', to: 'user_items#destroy', as: 'my_item'
+  resources :my_items, controller: 'user_items', only: %i[index destroy]
+  # get 'my_items', to: 'user_items#index', as: 'my_items'
+  post 'my_items(/:item_id)', to: 'user_items#create'
+  delete 'my_items/:id', to: 'user_items#destroy'
   get 'stores/results', to: 'stores#results'
 end
