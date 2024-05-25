e916fb3341ea7019ffc1752f41afde4c309fdae4
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-05-25 17:16:12 -0400

    made requested changes

--- db/migrate/20240525210424_change_price_to_decimal.rb
+++ db/migrate/20240525210424_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[7.1]
   def change
-    change_column :store_items, :price, :decimal
+    change_column :store_items, :price, :decimal, precision: 5, scale: 2
   end
 end
