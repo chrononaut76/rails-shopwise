8960c06b514ebd2aa6657542526874fdbbfbb051
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-05-28 19:57:13 -0400

    markers troubleshooting

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,7 +27,7 @@ export default class extends Controller {
     )};
 
     #fitMapToMarkers() {
-      const bounds = new mapboxgl.LngLatBounds()
+      const bounds = new mapboxgl.LngLatBounds();
       this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
       this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
     };
