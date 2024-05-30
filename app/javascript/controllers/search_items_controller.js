31c6c257b6d5fb1eb7b30d50f8ebd7e3bb79dbe6
karim issa <issa61139@gmail.com>
2024-05-30 14:59:29 -0400

    styled teh search bar but not final

--- app/javascript/controllers/search_items_controller.js
+++ app/javascript/controllers/search_items_controller.js
@@ -2,14 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-items"
 export default class extends Controller {
-  static targets = ['form', 'input', 'results'];
+  static targets = ['form', 'input', 'results', 'bar'];
 
   results() {
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`;
-    fetch(url, { headers: { 'Accept': 'text/plain' } })
-      .then(response => response.text())
-      .then(data => {
-        this.resultsTarget.outerHTML = data;
-      });
+    const query = this.inputTarget.value.trim().toLowerCase();
+    if (query.length > 0) {
+      const url = `${this.formTarget.action}?query=${query}`;
+      fetch(url, { headers: { 'Accept': 'text/plain' } })
+        .then(response => response.text())
+        .then(data => {
+          this.resultsTarget.innerHTML = data;
+          this.resultsTarget.classList.add('visible');
+          this.barTarget.classList.add('results-visible');
+        });
+    } else {
+      this.resultsTarget.classList.remove('visible');
+      this.barTarget.classList.remove('results-visible');
+    }
+  }
+
+  submitForm(event) {
+    event.preventDefault();
   }
 }
