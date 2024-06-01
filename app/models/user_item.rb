d981f714f2d9e5e53a5922d47c72f00661af76c0
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-06-01 13:50:52 -0500

    Adding AI request to UserItem model

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -3,4 +3,25 @@ class UserItem < ApplicationRecord
   belongs_to :item
 
   validates :user_id, :item_id, presence: true
+
+  private
+
+  def recipes
+    ingredients = []
+    @user_items = UserItem.find_by(current_user)
+    @user_items.each do |user_item|
+      ingredients << Item.find_by(user_item).name
+    end
+    Rails.cache.fetch("#{cache_key_with_version}/recipes") do
+      client = OpenAI::Client.new
+      response = client.chat(parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{
+          role: "user",
+          content: "Recommend some simple recipes with ingredients #{ingredients.join}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."
+        }]
+      })
+      response
+    end
+  end
 end
