4ae467e47f6f698b000ca0f672484fa81cb9b81e
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-06-02 22:25:49 -0400

    setting up meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,3 +19,7 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
+
+def default_url_options
+  { host: ENV[“DOMAIN”] || “localhost:3000” }
+  end
