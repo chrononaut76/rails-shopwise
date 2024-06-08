929d67ddd1b976d18897ae1101aad4f9b17a80fc
karim issa <issa61139@gmail.com>
2024-06-08 11:01:48 -0400

    working on popups to show up from the bottom

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
     });
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.#setupPopupInteraction()
   }
 
   #setMarkerColor(marker) {
@@ -26,27 +27,48 @@ export default class extends Controller {
       color = "#FF0000";
     } else if (marker.price_category === 'affordable') {
       color = "#0A4D71";
-    };
+    }
     return color;
-  };
+  }
+
+  #generatePopupHTML(marker) {
+    return marker.info_window_html;
+  }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      const popupHTML = this.#generatePopupHTML(marker);
 
-      new mapboxgl.Marker({"color": this.#setMarkerColor(marker)})
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(this.map)
-
-
-    }
-  )};
+      new mapboxgl.Marker({ "color": this.#setMarkerColor(marker) })
+        .setLngLat([marker.lng, marker.lat])
+        .getElement()
+        .addEventListener('click', () => this.#showPopup(popupHTML))
+        .addTo(this.map);
+    });
+  }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  };
+  }
 
+  #setupPopupInteraction() {
+    const popupClose = document.getElementById('popup-close');
+    popupClose.addEventListener('click', this.#hidePopup.bind(this));
+  }
+
+  #showPopup(content) {
+    const popupBody = document.getElementById('popup-body');
+    popupBody.innerHTML = content;
+    const popup = document.getElementById('map-popup');
+    popup.classList.remove('hidden');
+    popup.classList.add('show');
+  }
+
+  #hidePopup() {
+    const popup = document.getElementById('map-popup');
+    popup.classList.remove('show');
+    popup.classList.add('hidden');
+  }
 }
