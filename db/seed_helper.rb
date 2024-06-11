5dcdd10ab356ec0f07922d4e762c1c3216c9c053
sofofos <sofia.alvarez-gonzalez@potloc.com>
2024-06-11 13:30:51 -0400

    apply suggested fixes

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -1,9 +1,11 @@
 require 'open-uri'
 require 'json'
-require 'dotenv'
 
-Dotenv.load('.env') # run the script from the repo root: ruby db/seed_helper.rb
-filters= "&diet=balanced&dishType=Bread&dishType=Cereals&dishType=Main%20course&dishType=Preps&dishType=Salad&dishType=Sandwiches&dishType=Side%20dish&dishType=Soup&dishType=Starter&imageSize=THUMBNAIL"
+# run the script from the repo root: ruby db/seed_helper.rb
+# require 'dotenv' # uncomment to use in development
+# Dotenv.load('.env')
+
+filters = "&diet=balanced&dishType=Bread&dishType=Cereals&dishType=Main%20course&dishType=Preps&dishType=Salad&dishType=Sandwiches&dishType=Side%20dish&dishType=Soup&dishType=Starter&imageSize=THUMBNAIL"
 url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{ENV.fetch('EDAMAM_RECIPE_API_APP_ID')}&app_key=#{ENV.fetch('EDAMAM_RECIPE_API_KEY')}#{filters}"
 4.times do |index|
   response = URI.open(url).read
