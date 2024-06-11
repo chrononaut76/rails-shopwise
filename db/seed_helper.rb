44dc373a6a7caa3f4fdee3a3b7e959b79027b079
sofofos <sofia.alvarez-gonzalez@potloc.com>
2024-06-11 13:42:08 -0400

    disable dot env

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 require 'json'
 
 # run the script from the repo root: ruby db/seed_helper.rb
-require 'dotenv' # uncomment to use in development
-Dotenv.load('.env')
+# require 'dotenv' # uncomment to use in development
+# Dotenv.load('.env')
 
 filters = "&diet=balanced&dishType=Bread&dishType=Cereals&dishType=Main%20course&dishType=Preps&dishType=Salad&dishType=Sandwiches&dishType=Side%20dish&dishType=Soup&dishType=Starter&imageSize=THUMBNAIL"
 url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{ENV.fetch('EDAMAM_RECIPE_API_APP_ID')}&app_key=#{ENV.fetch('EDAMAM_RECIPE_API_KEY')}#{filters}"
